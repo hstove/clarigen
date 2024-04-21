47cb94a4e32eb323cfab5808f620d4d25d5d6df5
Hank Stoever <hstove@gmail.com>
2024-04-20 18:09:56 -0700

    feat: init config command

--- packages/cli/src/run-cli.ts
+++ packages/cli/src/run-cli.ts
@@ -2,6 +2,7 @@ import { Cli, Builtins } from 'clipanion';
 import { SessionInfoCommand } from './commands/session-info';
 import { DefaultCommand } from './commands/default-command';
 import { DocsCommand } from './commands/docs-command';
+import { InitConfigCommand } from './commands/init-config-command';
 
 const [node, script, ...args] = process.argv;
 
@@ -15,6 +16,7 @@ cli.register(Builtins.HelpCommand);
 cli.register(Builtins.VersionCommand);
 cli.register(DefaultCommand);
 cli.register(DocsCommand);
+cli.register(InitConfigCommand);
 cli.register(SessionInfoCommand);
 
 async function run() {
