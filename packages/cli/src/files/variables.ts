45196317c37edc7d397dbe492059cc20c84a275e
100proof <security@100proof.org>
2025-10-24 16:17:45 +1000

    Upgrade to Clarity 4 and bump to version 3.2.0

--- packages/cli/src/files/variables.ts
+++ packages/cli/src/files/variables.ts
@@ -47,7 +47,7 @@ export function getVariablesV2(contract: SessionContract, simnet: Simnet, verbos
       fakeId,
       fullSrc,
       {
-        clarityVersion: 3,
+        clarityVersion: 4,
       },
       deployer
     );
