04a874220252c917bd2baa240b1afad19e2504f1
Hank Stoever <1109058+hstove@users.noreply.github.com>
2025-12-17 06:18:59 -0800

    feat: nuqs

--- web/src/router.tsx
+++ web/src/router.tsx
@@ -1,13 +1,14 @@
-import { createRouter } from '@tanstack/react-router'
-import { setupRouterSsrQueryIntegration } from '@tanstack/react-router-ssr-query'
-import * as TanstackQuery from './integrations/tanstack-query/root-provider'
+import { createRouter } from '@tanstack/react-router';
+import { setupRouterSsrQueryIntegration } from '@tanstack/react-router-ssr-query';
+import * as TanstackQuery from './integrations/tanstack-query/root-provider';
+import { NuqsAdapter } from 'nuqs/adapters/tanstack-router';
 
 // Import the generated route tree
-import { routeTree } from './routeTree.gen'
+import { routeTree } from './routeTree.gen';
 
 // Create a new router instance
 export const getRouter = () => {
-  const rqContext = TanstackQuery.getContext()
+  const rqContext = TanstackQuery.getContext();
 
   const router = createRouter({
     routeTree,
@@ -16,13 +17,13 @@ export const getRouter = () => {
     Wrap: (props: { children: React.ReactNode }) => {
       return (
         <TanstackQuery.Provider {...rqContext}>
-          {props.children}
+          <NuqsAdapter>{props.children}</NuqsAdapter>
         </TanstackQuery.Provider>
-      )
+      );
     },
-  })
+  });
 
-  setupRouterSsrQueryIntegration({ router, queryClient: rqContext.queryClient })
+  setupRouterSsrQueryIntegration({ router, queryClient: rqContext.queryClient });
 
-  return router
-}
+  return router;
+};
