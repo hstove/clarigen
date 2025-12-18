1a159e05d5daec5309e2f7ecee3fed6be4d5c2f4
Hank Stoever <1109058+hstove@users.noreply.github.com>
2025-12-17 20:13:40 -0800

    feat: port 2527

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -7,6 +7,9 @@ import tailwindcss from '@tailwindcss/vite';
 import { nitro } from 'nitro/vite';
 
 const config = defineConfig({
+  server: {
+    port: 2527,
+  },
   plugins: [
     devtools(),
     nitro(),
